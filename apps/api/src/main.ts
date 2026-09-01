b858bad394f645ea2a90ac6b176783ea8911c8f8
Amirhossein <amirhossein611@gmail.com>
2026-09-01 14:29:05 +0330

    feat(api): add swagger documentation freeze

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { configureApp, configureHttpServer, getListenOptions } from './app.setup';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const bootstrapLogger = new Logger('Bootstrap');
 
@@ -9,6 +10,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   configureApp(app);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Hamidian Silver API')
+    .setDescription('Backend API documentation')
+    .setVersion('1.0')
+    .build();
+
+  SwaggerModule.setup('docs', app, SwaggerModule.createDocument(app, swaggerConfig));
   app.enableShutdownHooks();
 
   const { host, port } = getListenOptions(app);
